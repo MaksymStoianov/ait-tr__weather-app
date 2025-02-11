31868e1c34fa6e477b12e106b868cf8abf5817ae
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-02-11 12:33:02 +0100

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,20 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
-import path from 'path';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias:
-    {
-      'assets': path.resolve(__dirname, 'src/assets'),
-      'components': path.resolve(__dirname, 'src/components'),
-      'pages': path.resolve(__dirname, 'src/pages'),
-      'styles': path.resolve(__dirname, 'src/styles'),
-      'store': path.resolve(__dirname, 'src/store')
-    }
+    alias: {
+      assets: path.resolve(__dirname, "src/assets"),
+      components: path.resolve(__dirname, "src/components"),
+      pages: path.resolve(__dirname, "src/pages"),
+      store: path.resolve(__dirname, "src/store"),
+      styles: path.resolve(__dirname, "src/styles"),
+    },
   },
   server: {
     open: true,
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
-})
+});
