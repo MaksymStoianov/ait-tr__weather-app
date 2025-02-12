ae668b4788ea04a06cdcdda07b1e5d6462a12720
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-02-12 14:52:55 +0100

    refactor(pages/NotFound)

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
 import { StyledPageWrapper } from "./styles";
 
 function NotFound() {
-  return <StyledPageWrapper></StyledPageWrapper>;
+  return (
+    <StyledPageWrapper>
+      <h1>404</h1>
+      <p>Oops! Page not found.</p>
+      <p>
+        <Link to="/">Go back to Home</Link>
+      </p>
+    </StyledPageWrapper>
+  );
 }
 
 export default NotFound;
--- src/pages/NotFound/styles.ts
+++ src/pages/NotFound/styles.ts
@@ -4,5 +4,7 @@ export const StyledPageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   flex: 1;
+  gap: 10px;
 `;
